21d6568c09c5f909926790b46f2a90debd97fba0
Gobindroop Mann <gobindroopm@gmail.com>
2019-11-12 22:40:56 -0800

    Added presets for sparse, dense, stable, and volatile

--- presets.js
+++ presets.js
@@ -1,23 +1,17 @@
 function preset_sparse() {
-	document.getElementById("cellGrowthRate").value = 999;
-	document.getElementById("cellDecayRate").value = 999;
-	document.getElementById("cellSpawn").value = 999;
+	document.getElementById("cellGrowthRate").value = 0.2;
+	document.getElementById("cellDecayRate").value = 0.005;
 }
 
 function preset_dense() {
-	document.getElementById("cellGrowthRate").value = 999;
-	document.getElementById("cellDecayRate").value = 999;
-	document.getElementById("cellSpawn").value = 999;
+	document.getElementById("cellGrowthRate").value = 0.8;
+	document.getElementById("cellDecayRate").value = 0.002;
 }
 function preset_stable() {
-	document.getElementById("cellGrowthRate").value = 999;
-	document.getElementById("cellDecayRate").value = 999;
-	document.getElementById("cellSpawn").value = 999;
-	document.getElementById("cellOffspringVolatility").value = 999;
+	document.getElementById("cellSpawn").value = 0.0005;
+	document.getElementById("cellOffspringVolatility").value = 0.005;
 }
 function preset_volatile() {
-	document.getElementById("cellGrowthRate").value = 999;
-	document.getElementById("cellDecayRate").value = 999;
-	document.getElementById("cellSpawn").value = 999;
-	document.getElementById("cellOffspringVolatility").value = 999;
+	document.getElementById("cellSpawn").value = 0.01;
+	document.getElementById("cellOffspringVolatility").value = 0.15;
 }
